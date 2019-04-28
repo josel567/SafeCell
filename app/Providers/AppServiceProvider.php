d99d99db041a5faf72b2025693dc1a9659fdcd78
Jose Luis <joseluisgarcia567@gmail.com>
2019-04-28 19:03:06 +0200

    fixing secure assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Contracts\Routing\UrlGenerator;
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if(env('APP_ENV') == 'production')
         {
-            $url->forceSchema('https');
+            $url->forceScheme('https');
         }
     }
 }
