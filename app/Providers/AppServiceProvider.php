4fac03232cfa75642c4784b8662c089db15c4a37
Jose Luis <joseluisgarcia567@gmail.com>
2019-04-28 18:51:50 +0200

    fixing secure assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        if(env('production'))
+        if(env('APP_ENV') == 'production')
         {
             $url->forceSchema('https');
         }
