b03437ac038b50e3fc46c8961076e374afb7d33f
Jose Luis <joseluisgarcia567@gmail.com>
2019-04-28 18:54:40 +0200

    fixing secure assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Routing\UrlGenerator;
+use Illuminate\Contracts\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
