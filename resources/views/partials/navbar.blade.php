362a6fb19fb5d8891b751a3054d9764178dcd44c
iaw46466489_Iglesias_Baladon_Iris <irisigba@gmail.com>
2019-05-17 13:31:44 +0200

    arreglo navBar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -4,7 +4,7 @@
 <div class="dashboard-header">
     <nav class="navbar navbar-expand-lg bg-white fixed-top ">
         <a class="navbar-brand " href="/"> <img class="nav-logo" src="{{ asset('images/logo_safecell_alargado.png') }}"/></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button  id="btn-navbar" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse " id="navbarSupportedContent">
